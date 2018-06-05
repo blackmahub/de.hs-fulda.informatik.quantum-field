b75317c5c32380a30e2781f6f47fef853af9cbfb
mahbubur.rahman <blackmahbub@gmail.com>
2018-06-05 20:40:26 +0200

    convolution in tensorflow

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ CONST_ConvKernelz [0,0,0,:] = [1,-2,1]
 # initial empty lattice. This will be dynamically generated later
 arr = np.empty([CONST_Dimension1, CONST_Dimension2, CONST_Dimension3, CONST_Dimension4])
 
+
 # random lattice generation
 def define_lattice():
     global arr 
@@ -71,7 +72,7 @@ def calculate_action_loop():
                     S += tFactor + xFactor + yFactor + zFactor - (8 * arr[t,x,y,z]) + (CONST_m**2/2)*(arr[t,x,y,z]**2)
     
     S /= CONST_Volume
-    print("S_Loop: "+ str(S))
+    # print("S_Loop: "+ str(S))
     return S
 
 
@@ -95,7 +96,7 @@ def calculate_action_roll():
     total = tLeft + tRight + xLeft + xRight + yLeft + yRight + zLeft + zRight + common    
     S= total.sum() / CONST_Volume
 
-    print("S_Roll: " + str(S))
+    # print("S_Roll: " + str(S))
     return S
 
 
@@ -122,9 +123,12 @@ def define_tf_graph():
     #S = tF.Print(S,[S], message="T_Roll: ")
     return S,arr
 
-def calculate_action_tf(sess,S,placeholder,data):
-  return sess.run([S],feed_dict = {placeholder:data})[0]
-  
+
+def calculate_action_tf(sess, S, placeholder_dict, action_name):
+    tf_action = sess.run([S], feed_dict=placeholder_dict)[0]
+    # print("S_TF_%s: %s" % (action_name, str(tf_action)))
+    return tf_action
+
 
 # calculating using convolution
 def calculate_action_convolve():
@@ -134,12 +138,37 @@ def calculate_action_convolve():
     convarrz = sp.ndimage.filters.convolve(arr, CONST_ConvKernelz, mode = 'wrap')
     common =  arr * ((CONST_m**2/2) * arr)
     S = (convarrt + convarrx + convarry + convarrz + common).sum() / CONST_Volume
-    print("S_Convolve: " + str(S))
+    # print("S_Convolve: " + str(S))
     return S
+
+def define_tf_convolution():
+    placeholder_arr = tF.placeholder(tF.float32)
+    placeholder_kernel_t = tF.placeholder(tF.float32)
+    placeholder_kernel_x = tF.placeholder(tF.float32)
+    placeholder_kernel_y = tF.placeholder(tF.float32)
+    placeholder_kernel_z = tF.placeholder(tF.float32)
+    placeholder_imag_zeros = tF.placeholder(tF.float32)
+
+    arr_fft = tF.fft(tF.complex(placeholder_arr, placeholder_imag_zeros))
+    kernel_t_fft = tF.fft(tF.complex(placeholder_kernel_t, placeholder_imag_zeros))
+    kernel_x_fft = tF.fft(tF.complex(placeholder_kernel_x, placeholder_imag_zeros))
+    kernel_y_fft = tF.fft(tF.complex(placeholder_kernel_y, placeholder_imag_zeros))
+    kernel_z_fft = tF.fft(tF.complex(placeholder_kernel_z, placeholder_imag_zeros))
+
+    conv_axis_0 = tF.real(tF.ifft(arr_fft * kernel_t_fft))
+    conv_axis_1 = tF.real(tF.ifft(arr_fft * kernel_x_fft))
+    conv_axis_2 = tF.real(tF.ifft(arr_fft * kernel_y_fft))
+    conv_axis_3 = tF.real(tF.ifft(arr_fft * kernel_z_fft))
+
+    common = placeholder_arr * ((CONST_m ** 2 / 2) * placeholder_arr)
+    conv_action = tF.reduce_sum(conv_axis_0 + conv_axis_1 + conv_axis_2 + conv_axis_3 + common) / CONST_Volume
+
+    return conv_action, placeholder_arr, placeholder_kernel_t, placeholder_kernel_x, \
+           placeholder_kernel_y, placeholder_kernel_z, placeholder_imag_zeros
 
 
 # plot comparison graph
-def plot_graph(volumes, loop_times, roll_times, tensor_roll_times, conv_times):
+def plot_graph(volumes, loop_times, roll_times, tensor_roll_times, conv_times, tensor_conv_times):
     n_groups = len(volumes)
     fig, ax = plt.subplots()
     index = np.arange(n_groups)
@@ -162,6 +191,10 @@ def plot_graph(volumes, loop_times, roll_times, tensor_roll_times, conv_times):
                     alpha=opacity, color='c',
                     label='SciPy Convolve')
 
+    rects5 = ax.bar(index + 4 * bar_width, tensor_conv_times, bar_width,
+                    alpha=opacity, color='y',
+                    label='Tensorflow Convolve')
+
     ax.set_xlabel('Volume')
     ax.set_ylabel('Time (in s)')
     ax.set_title('Times by Volumes')
@@ -174,8 +207,10 @@ def plot_graph(volumes, loop_times, roll_times, tensor_roll_times, conv_times):
     autolabel(ax, rects2)
     autolabel(ax, rects3)
     autolabel(ax, rects4)
+    autolabel(ax, rects5)
 
     plt.show()
+
 
 # Attach a text label above each bar displaying its height
 def autolabel(ax, rects):
@@ -185,10 +220,13 @@ def autolabel(ax, rects):
                 str(round(height, 3)),
                 ha='center', va='bottom')
 
+
 # main function
 def main():
     define_lattice()
     STF,plTF = define_tf_graph()
+    tf_conv, placeholder_conv, placeholder_kernel_t, placeholder_kernel_x, \
+    placeholder_kernel_y, placeholder_kernel_z, placeholder_imag_zeros = define_tf_convolution()
     volumes = []
     loop_actions = []
     loop_times = []
@@ -198,6 +236,8 @@ def main():
     tensor_roll_times = []
     conv_actions = []
     conv_times = []
+    tensor_conv_actions = []
+    tensor_conv_times = []
     for count in range(3):
 
         print("Dimension: " + str(arr.ndim))
@@ -209,31 +249,55 @@ def main():
         loop_actions.append(calculate_action_loop())
         tf = time.time() - t0
         loop_times.append(tf)
-        print("Loop_Time: "+ str(tf))
+        # print("Loop_Time: "+ str(tf))
 
         t0 = time.time()
         roll_actions.append(calculate_action_roll())
         tf = time.time() - t0
         roll_times.append(tf)
-        print("Roll_Time: "+ str(tf))
+        # print("Roll_Time: "+ str(tf))
 
         t0 = time.time()        
-        tensor_roll_actions.append(calculate_action_tf(sess,STF,plTF,arr))
+        tensor_roll_actions.append(calculate_action_tf(sess, STF, {plTF: arr}, "Roll"))
         tf = time.time() - t0
         tensor_roll_times.append(tf)    
-        print("Tensor_Time: "+ str(tf))
+        # print("Tensor_Roll_Time: "+ str(tf))
 
-        t0 = time.time()        
+        t0 = time.time()
         conv_actions.append(calculate_action_convolve())
         tf = time.time() - t0
-        conv_times.append(tf)    
-        print("Conv_Time: "+ str(tf))
+        conv_times.append(tf)
+        # print("Conv_Time: "+ str(tf))
+
+        t0 = time.time()
+        kernel_t = np.zeros(arr.shape, dtype=np.float32)
+        kernel_t[0:3, 0, 0, 0] = [1, -2, 1]
+        kernel_x = np.zeros(arr.shape, dtype=np.float32)
+        kernel_x[0, 0:3, 0, 0] = [1, -2, 1]
+        kernel_y = np.zeros(arr.shape, dtype=np.float32)
+        kernel_y[0, 0, 0:3, 0] = [1, -2, 1]
+        kernel_z = np.zeros(arr.shape, dtype=np.float32)
+        kernel_z[0, 0, 0, 0:3] = [1, -2, 1]
+        imag_zeros = np.zeros(arr.shape, dtype=np.float32)
+        placeholder_dict = {
+            placeholder_conv: arr,
+            placeholder_kernel_t: kernel_t,
+            placeholder_kernel_x: kernel_x,
+            placeholder_kernel_y: kernel_y,
+            placeholder_kernel_z: kernel_z,
+            placeholder_imag_zeros: imag_zeros
+        }
+        tensor_conv_actions.append(calculate_action_tf(sess, tf_conv, placeholder_dict, "Convolution"))
+        tf = time.time() - t0
+        tensor_conv_times.append(tf)
+        # print("Tensor_Convolution_Time: " + str(tf))
         print()
 
         increase_dimension_sizes(1.5)
 
-    plot_graph(volumes, loop_times, roll_times, tensor_roll_times, conv_times)
-        
-            
+    plot_graph(volumes, loop_times, roll_times, tensor_roll_times, conv_times, tensor_conv_times)
+    sess.close()
+
+
 if __name__ == "__main__":
     main()
