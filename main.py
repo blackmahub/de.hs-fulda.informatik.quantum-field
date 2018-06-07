ff9ebefb8dd78d6c5c87b719f92ef44221097a6e
Swetaketu Majumder <swetaketum@gmail.com>
2018-06-07 12:54:02 +0200

    5 Dimension (except TF)

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import tensorflow as tF
 sess = tF.InteractiveSession()
 
 # initial dimension sizes
+CONST_N = 100
 CONST_Dimension1 = 20
 CONST_Dimension2 = 20
 CONST_Dimension3 = 20
@@ -32,13 +33,14 @@ CONST_ConvKernelz = np.zeros([1,1,1,3])
 CONST_ConvKernelz [0,0,0,:] = [1,-2,1]
 
 # initial empty lattice. This will be dynamically generated later
-arr = np.empty([CONST_Dimension1, CONST_Dimension2, CONST_Dimension3, CONST_Dimension4])
+arr = np.empty([CONST_N, CONST_Dimension1, CONST_Dimension2, CONST_Dimension3, CONST_Dimension4])
 
 
 # random lattice generation
 def define_lattice():
-    global arr 
-    arr = np.random.uniform(CONST_FieldMin, CONST_FieldMax, size = (CONST_Dimension1, CONST_Dimension2, CONST_Dimension3, CONST_Dimension4))    
+    global arr
+    for n in range(CONST_N): 
+        arr[n,:,:,:] = np.random.uniform(CONST_FieldMin, CONST_FieldMax, size = (CONST_Dimension1, CONST_Dimension2, CONST_Dimension3, CONST_Dimension4))    
 
 
 # increasing volume of lattice for comparison
@@ -56,48 +58,53 @@ def increase_dimension_sizes(factor):
 
 # calculating action by nested for loops, normalizing by volume
 def calculate_action_loop():
-    S = 0
-    for t in range(CONST_Dimension1):
-        for x in range(CONST_Dimension2):
-            for y in range(CONST_Dimension3):
-                for z in range(CONST_Dimension4):
-                    tFactor = (arr[t+1, x, y, z] if t < CONST_Dimension1 - 1 else arr[0,x,y,z]) + (arr[t-1, x, y, z] if t > 0 else arr[CONST_Dimension1 - 1, x, y, z]) #- (2 * arr[t,x,y,z])
-                    xFactor = (arr[t, x+1, y, z] if x < CONST_Dimension2 - 1 else arr[t,0,y,z]) + (arr[t, x-1, y, z] if x > 0 else arr[t, CONST_Dimension2 - 1, y, z]) #- (2 * arr[t,x,y,z])
-                    yFactor = (arr[t, x, y+1, z] if y < CONST_Dimension3 - 1 else arr[t,x,0,z]) + (arr[t, x, y-1, z] if y > 0 else arr[t, x, CONST_Dimension3 - 1, z]) #- (2 * arr[t,x,y,z])
-                    zFactor = (arr[t, x, y, z+1] if z < CONST_Dimension4 - 1 else arr[t,x,y,0]) + (arr[t, x, y, z-1] if z > 0 else arr[t, x, y, CONST_Dimension4 - 1]) #- (2 * arr[t,x,y,z])
-                    # tFactor = (arr[t+1, x, y, z] if t < CONST_Dimension1 - 1 else 0) + (arr[t-1, x, y, z] if t > 0 else 0) - (2 * arr[t,x,y,z])
-                    # xFactor = (arr[t, x+1, y, z] if x < CONST_Dimension2 - 1 else 0) + (arr[t, x-1, y, z] if x > 0 else 0) - (2 * arr[t,x,y,z])
-                    # yFactor = (arr[t, x, y+1, z] if y < CONST_Dimension3 - 1 else 0) + (arr[t, x, y-1, z] if y > 0 else 0) - (2 * arr[t,x,y,z])
-                    # zFactor = (arr[t, x, y, z+1] if z < CONST_Dimension4 - 1 else 0) + (arr[t, x, y, z-1] if z > 0 else 0) - (2 * arr[t,x,y,z])
-                    S += tFactor + xFactor + yFactor + zFactor - (8 * arr[t,x,y,z]) + (CONST_m**2/2)*(arr[t,x,y,z]**2)
-    
-    S /= CONST_Volume
+    Sum = []
+    for n in range(CONST_N):
+        S = 0
+        for t in range(CONST_Dimension1):
+            for x in range(CONST_Dimension2):
+                for y in range(CONST_Dimension3):
+                    for z in range(CONST_Dimension4):
+                        tFactor = (arr[n, t+1, x, y, z] if t < CONST_Dimension1 - 1 else arr[n, 0,x,y,z]) + (arr[n, t-1, x, y, z] if t > 0 else arr[n, CONST_Dimension1 - 1, x, y, z]) #- (2 * arr[t,x,y,z])
+                        xFactor = (arr[n, t, x+1, y, z] if x < CONST_Dimension2 - 1 else arr[n, t,0,y,z]) + (arr[n, t, x-1, y, z] if x > 0 else arr[n, CONST_Dimension2 - 1, y, z]) #- (2 * arr[t,x,y,z])
+                        yFactor = (arr[n, t, x, y+1, z] if y < CONST_Dimension3 - 1 else arr[n, t,x,0,z]) + (arr[n, t, x, y-1, z] if y > 0 else arr[n, t, x, CONST_Dimension3 - 1, z]) #- (2 * arr[t,x,y,z])
+                        zFactor = (arr[n, t, x, y, z+1] if z < CONST_Dimension4 - 1 else arr[n, t,x,y,0]) + (arr[n, t, x, y, z-1] if z > 0 else arr[n, t, x, y, CONST_Dimension4 - 1]) #- (2 * arr[t,x,y,z])
+                        # tFactor = (arr[t+1, x, y, z] if t < CONST_Dimension1 - 1 else 0) + (arr[t-1, x, y, z] if t > 0 else 0) - (2 * arr[t,x,y,z])
+                        # xFactor = (arr[t, x+1, y, z] if x < CONST_Dimension2 - 1 else 0) + (arr[t, x-1, y, z] if x > 0 else 0) - (2 * arr[t,x,y,z])
+                        # yFactor = (arr[t, x, y+1, z] if y < CONST_Dimension3 - 1 else 0) + (arr[t, x, y-1, z] if y > 0 else 0) - (2 * arr[t,x,y,z])
+                        # zFactor = (arr[t, x, y, z+1] if z < CONST_Dimension4 - 1 else 0) + (arr[t, x, y, z-1] if z > 0 else 0) - (2 * arr[t,x,y,z])
+                        S += tFactor + xFactor + yFactor + zFactor - (8 * arr[n, t,x,y,z]) + (CONST_m**2/2)*(arr[n, t,x,y,z]**2)
+        S /= CONST_Volume
+        Sum.append(S)
     # print("S_Loop: "+ str(S))
-    return S
+    return Sum
 
 
 # calculating action by roll, normalizing by volume
 def calculate_action_roll():
+    Sum = []
+    for n in range(CONST_N):
+        S = 0
+        tLeft = np.roll(arr[n,:,:,:], -1, axis = 1)
+        tRight = np.roll(arr[n,:,:,:], 1, axis = 1)
 
-    tLeft = np.roll(arr, -1, axis = 1)
-    tRight = np.roll(arr, 1, axis = 1)
+        xLeft = np.roll(arr[n,:,:,:], -1, axis = 0)
+        xRight = np.roll(arr[n,:,:,:], 1, axis = 0)
 
-    xLeft = np.roll(arr, -1, axis = 0)
-    xRight = np.roll(arr, 1, axis = 0)
+        yLeft = np.roll(arr[n,:,:,:], -1, axis = 2)
+        yRight = np.roll(arr[n,:,:,:], 1, axis = 2)
 
-    yLeft = np.roll(arr, -1, axis = 2)
-    yRight = np.roll(arr, 1, axis = 2)
+        zLeft = np.roll(arr[n,:,:,:], -1, axis = 3)
+        zRight = np.roll(arr[n,:,:,:], 1, axis = 3)
 
-    zLeft = np.roll(arr, -1, axis = 3)
-    zRight = np.roll(arr, 1, axis = 3)
+        common =  arr[n,:,:,:] * (-8 + (CONST_m**2/2) * arr[n,:,:,:])
+        
+        total = tLeft + tRight + xLeft + xRight + yLeft + yRight + zLeft + zRight + common    
+        S= total.sum() / CONST_Volume
 
-    common =  arr * (-8 + (CONST_m**2/2) * arr)
-    
-    total = tLeft + tRight + xLeft + xRight + yLeft + yRight + zLeft + zRight + common    
-    S= total.sum() / CONST_Volume
-
+        Sum.append(S)
     # print("S_Roll: " + str(S))
-    return S
+    return Sum
 
 
 # calculating using tensorflow
@@ -132,14 +139,18 @@ def calculate_action_tf(sess, S, placeholder_dict, action_name):
 
 # calculating using convolution
 def calculate_action_convolve():
-    convarrt = sp.ndimage.filters.convolve(arr, CONST_ConvKernelt, mode = 'wrap')
-    convarrx = sp.ndimage.filters.convolve(arr, CONST_ConvKernelx, mode = 'wrap')
-    convarry = sp.ndimage.filters.convolve(arr, CONST_ConvKernely, mode = 'wrap')
-    convarrz = sp.ndimage.filters.convolve(arr, CONST_ConvKernelz, mode = 'wrap')
-    common =  arr * ((CONST_m**2/2) * arr)
-    S = (convarrt + convarrx + convarry + convarrz + common).sum() / CONST_Volume
-    # print("S_Convolve: " + str(S))
-    return S
+    Sum = []
+    for n in range(CONST_N):
+        S = 0
+        convarrt = sp.ndimage.filters.convolve(arr[n,:,:,:], CONST_ConvKernelt, mode = 'wrap')
+        convarrx = sp.ndimage.filters.convolve(arr[n,:,:,:], CONST_ConvKernelx, mode = 'wrap')
+        convarry = sp.ndimage.filters.convolve(arr[n,:,:,:], CONST_ConvKernely, mode = 'wrap')
+        convarrz = sp.ndimage.filters.convolve(arr[n,:,:,:], CONST_ConvKernelz, mode = 'wrap')
+        common =  arr[n,:,:,:] * ((CONST_m**2/2) * arr[n,:,:,:])
+        S = (convarrt + convarrx + convarry + convarrz + common).sum() / CONST_Volume
+        Sum.append(S)
+        # print("S_Convolve: " + str(S))
+    return Sum
 
 def define_tf_convolution():
     placeholder_arr = tF.placeholder(tF.float32)
@@ -246,54 +257,57 @@ def main():
         print("Shape: " + str(arr.shape))
 
         t0 = time.time()
-        loop_actions.append(calculate_action_loop())
+        calculate_action_loop()
+        # loop_actions.append(calculate_action_loop())
         tf = time.time() - t0
         loop_times.append(tf)
         # print("Loop_Time: "+ str(tf))
 
         t0 = time.time()
-        roll_actions.append(calculate_action_roll())
+        calculate_action_roll()
+        # roll_actions.append(calculate_action_roll())
         tf = time.time() - t0
         roll_times.append(tf)
         # print("Roll_Time: "+ str(tf))
 
-        t0 = time.time()        
-        tensor_roll_actions.append(calculate_action_tf(sess, STF, {plTF: arr}, "Roll"))
-        tf = time.time() - t0
-        tensor_roll_times.append(tf)    
+        # t0 = time.time()        
+        # tensor_roll_actions.append(calculate_action_tf(sess, STF, {plTF: arr}, "Roll"))
+        # tf = time.time() - t0
+        # tensor_roll_times.append(tf)    
         # print("Tensor_Roll_Time: "+ str(tf))
 
         t0 = time.time()
-        conv_actions.append(calculate_action_convolve())
+        calculate_action_convolve()
+        # conv_actions.append(calculate_action_convolve())
         tf = time.time() - t0
         conv_times.append(tf)
         # print("Conv_Time: "+ str(tf))
 
-        t0 = time.time()
-        kernel_t = np.zeros(arr.shape, dtype=np.float32)
-        kernel_t[0:3, 0, 0, 0] = [1, -2, 1]
-        kernel_x = np.zeros(arr.shape, dtype=np.float32)
-        kernel_x[0, 0:3, 0, 0] = [1, -2, 1]
-        kernel_y = np.zeros(arr.shape, dtype=np.float32)
-        kernel_y[0, 0, 0:3, 0] = [1, -2, 1]
-        kernel_z = np.zeros(arr.shape, dtype=np.float32)
-        kernel_z[0, 0, 0, 0:3] = [1, -2, 1]
-        imag_zeros = np.zeros(arr.shape, dtype=np.float32)
-        placeholder_dict = {
-            placeholder_conv: arr,
-            placeholder_kernel_t: kernel_t,
-            placeholder_kernel_x: kernel_x,
-            placeholder_kernel_y: kernel_y,
-            placeholder_kernel_z: kernel_z,
-            placeholder_imag_zeros: imag_zeros
-        }
-        tensor_conv_actions.append(calculate_action_tf(sess, tf_conv, placeholder_dict, "Convolution"))
-        tf = time.time() - t0
-        tensor_conv_times.append(tf)
+        # t0 = time.time()
+        # kernel_t = np.zeros(arr.shape, dtype=np.float32)
+        # kernel_t[0:3, 0, 0, 0] = [1, -2, 1]
+        # kernel_x = np.zeros(arr.shape, dtype=np.float32)
+        # kernel_x[0, 0:3, 0, 0] = [1, -2, 1]
+        # kernel_y = np.zeros(arr.shape, dtype=np.float32)
+        # kernel_y[0, 0, 0:3, 0] = [1, -2, 1]
+        # kernel_z = np.zeros(arr.shape, dtype=np.float32)
+        # kernel_z[0, 0, 0, 0:3] = [1, -2, 1]
+        # imag_zeros = np.zeros(arr.shape, dtype=np.float32)
+        # placeholder_dict = {
+        #     placeholder_conv: arr,
+        #     placeholder_kernel_t: kernel_t,
+        #     placeholder_kernel_x: kernel_x,
+        #     placeholder_kernel_y: kernel_y,
+        #     placeholder_kernel_z: kernel_z,
+        #     placeholder_imag_zeros: imag_zeros
+        # }
+        # tensor_conv_actions.append(calculate_action_tf(sess, tf_conv, placeholder_dict, "Convolution"))
+        # tf = time.time() - t0
+        # tensor_conv_times.append(tf)
         # print("Tensor_Convolution_Time: " + str(tf))
         print()
 
-        increase_dimension_sizes(1.5)
+        # increase_dimension_sizes(1.5)
 
     plot_graph(volumes, loop_times, roll_times, tensor_roll_times, conv_times, tensor_conv_times)
     sess.close()
